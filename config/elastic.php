8d7031c26b23414fdf973f6d93d026a3de07121d
Thomas Wijnands <twijnands@swis.nl>
2023-06-21 12:04:40 +0200

    fix: remove indexmapping from config

--- config/elastic.php
+++ config/elastic.php
@@ -8,48 +8,6 @@ return [
         // define your model classes here that needs to be indexed
     ],
     'index_mapping' => [
-        'settings' => [
-            'analysis' => [
-                'filter' => [
-                    'stop_nl' => [
-                        'type' => 'stop',
-                        'stopwords' => '_dutch_',
-                    ],
-                    'snow_nl' => [
-                        'type' => 'snowball',
-                        'language' => 'Dutch',
-                    ],
-                    'length_nl' => [
-                        'type' => 'length',
-                        'min' => 3,
-                    ],
-                ],
-                'analyzer' => [
-                    'default' => [
-                        'tokenizer' => 'standard',
-                        'filter' => [
-                            'lowercase',
-                            'length_nl',
-                            'stop_nl',
-                            'snow_nl',
-                        ],
-                    ],
-                    'default_search' => [
-                        'tokenizer' => 'standard',
-                        'filter' => [
-                            'lowercase',
-                            'length_nl',
-                            'stop_nl',
-                            'snow_nl',
-                        ],
-                    ],
-                ],
-            ],
-        ],
-        'mappings' => [
-            'properties' => [
-                'type' => ['type' => 'keyword'],
-            ],
-        ],
+        // define your index mapping here
     ],
 ];
