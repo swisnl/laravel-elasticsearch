73435288593cff25c1e91869d35fb99ac348154c
Jasper Zonneveld <jasper@swis.nl>
2025-02-21 14:21:18 +0100

    docs: fix typo

--- config/elasticsearch.php
+++ config/elasticsearch.php
@@ -10,7 +10,7 @@ return [
         // define your model classes here that needs to be indexed
     ],
     'search_fields' => [
-        // define the fields that needs to be searched with aditional boosting example: 'title^5'
+        // define the fields that needs to be searched with additional boosting example: 'title^5'
     ],
     'index_mapping' => [
         'mappings' => [
